7882c7b291db42c5ba0ce34144b8ba8891489aae
Matus Olejnik <matus.olejnik@gmail.com>
2021-12-19 04:32:30 +0100

    feat: change post endpoint to accept file upload

--- src/main/java/com/devmo/autogradingbe/controller/rest/MainController.java
+++ src/main/java/com/devmo/autogradingbe/controller/rest/MainController.java
@@ -1,23 +1,35 @@
 package com.devmo.autogradingbe.controller.rest;
 
-import com.devmo.autogradingbe.bm.BuildOutputRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class MainController {
 
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
-    @PostMapping("/build-outputs")
-    public ResponseEntity<?> processBuildOutput(@RequestBody BuildOutputRequest request) {
+    @PostMapping("/{studentId}/build-outputs")
+    public ResponseEntity<?> processBuildOutput(
+            @PathVariable String studentId,
+            @RequestParam("output-file") MultipartFile file) {
 
-        logger.info("Processing build output: " + request);
+        logger.info(String.format("Processing build output: studentId=%s, fileName=%s", studentId, file.getName()));
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+            bufferedReader.lines().forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return ResponseEntity.ok().build();
     }
